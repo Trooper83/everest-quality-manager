2adb55d8dab6ca088c87606d0692c9d2fbba7beb
Scot Noftz <scot.noftz@ensono.com>
2021-12-14 21:06:14 -0800

    updated editTestCase function

--- src/integration-test/groovy/com/everlution/test/ui/specs/testcase/edit/EditPageSpec.groovy
+++ src/integration-test/groovy/com/everlution/test/ui/specs/testcase/edit/EditPageSpec.groovy
@@ -6,7 +6,6 @@ import com.everlution.test.ui.support.pages.common.HomePage
 import com.everlution.test.ui.support.pages.common.LoginPage
 import com.everlution.test.ui.support.pages.testcase.EditTestCasePage
 import com.everlution.test.ui.support.pages.testcase.ListTestCasePage
-import com.everlution.test.ui.support.pages.testcase.ShowTestCasePage
 import geb.spock.GebSpec
 import grails.testing.mixin.integration.Integration
 
@@ -15,23 +14,15 @@ class EditPageSpec extends GebSpec {
 
     TestCaseService testCaseService
 
-    void "home link directs to home view"() {
-        given: "login as a basic user"
+    def setup() {
+        def id = testCaseService.list(max: 1).first().id
         to LoginPage
         LoginPage loginPage = browser.page(LoginPage)
         loginPage.login(Usernames.BASIC.username, "password")
+        go "/testCase/edit/${id}"
+    }
 
-        and: "go to list test case page"
-        to ListTestCasePage
-
-        and: "click first test case in list"
-        ListTestCasePage listPage = browser.page(ListTestCasePage)
-        listPage.testCaseTable.clickCell("Name", 0)
-
-        and: "go to edit"
-        ShowTestCasePage showPage = browser.page(ShowTestCasePage)
-        showPage.goToEdit()
-
+    void "home link directs to home view"() {
         when: "click the home button"
         EditTestCasePage page = browser.page(EditTestCasePage)
         page.goToHome()
@@ -41,22 +32,6 @@ class EditPageSpec extends GebSpec {
     }
 
     void "list link directs to list view"() {
-        given: "login as a basic user"
-        to LoginPage
-        LoginPage loginPage = browser.page(LoginPage)
-        loginPage.login(Usernames.BASIC.username, "password")
-
-        and: "go to list test case page"
-        to ListTestCasePage
-
-        and: "click first test case in list"
-        ListTestCasePage listPage = browser.page(ListTestCasePage)
-        listPage.testCaseTable.clickCell("Name", 0)
-
-        and: "go to edit"
-        ShowTestCasePage showPage = browser.page(ShowTestCasePage)
-        showPage.goToEdit()
-
         when: "click the list link"
         EditTestCasePage page = browser.page(EditTestCasePage)
         page.goToList()
@@ -66,59 +41,18 @@ class EditPageSpec extends GebSpec {
     }
 
     void "correct fields are displayed"() {
-        given: "login as basic user"
-        to LoginPage
-        LoginPage loginPage = browser.page(LoginPage)
-        loginPage.login(Usernames.BASIC.username, "password")
-
-        when: "go to edit page"
-        def id = testCaseService.list(max: 1).first().id
-        go "/testCase/edit/${id}"
-
-        then: "correct fields are displayed"
+        expect: "correct fields are displayed"
         EditTestCasePage page = browser.page(EditTestCasePage)
         page.getFields() == ["Project", "Area", "Environments", "Description", "Execution Method *", "Name *", "Type *"]
     }
 
     void "required fields indicator displayed for required fields"() {
-        given: "login as a basic user"
-        to LoginPage
-        LoginPage loginPage = browser.page(LoginPage)
-        loginPage.login(Usernames.BASIC.username, "password")
-
-        and: "go to list test case page"
-        to ListTestCasePage
-
-        and: "click first test case in list"
-        ListTestCasePage listPage = browser.page(ListTestCasePage)
-        listPage.testCaseTable.clickCell("Name", 0)
-
-        and: "go to edit"
-        ShowTestCasePage showPage = browser.page(ShowTestCasePage)
-        showPage.goToEdit()
-
         expect: "required field indicators displayed"
         EditTestCasePage page = browser.page(EditTestCasePage)
         page.areRequiredFieldIndicatorsDisplayed(["executionMethod", "name", "type"])
     }
 
     void "verify method and type field options"() {
-        given: "login as a basic user"
-        to LoginPage
-        LoginPage loginPage = browser.page(LoginPage)
-        loginPage.login(Usernames.BASIC.username, "password")
-
-        and: "go to list test case page"
-        to ListTestCasePage
-
-        and: "click first test case in list"
-        ListTestCasePage listPage = browser.page(ListTestCasePage)
-        listPage.testCaseTable.clickCell("Name", 0)
-
-        and: "go to edit"
-        ShowTestCasePage showPage = browser.page(ShowTestCasePage)
-        showPage.goToEdit()
-
         expect: "correct options populate for executionMethod and type"
         EditTestCasePage page = browser.page(EditTestCasePage)
         verifyAll {
